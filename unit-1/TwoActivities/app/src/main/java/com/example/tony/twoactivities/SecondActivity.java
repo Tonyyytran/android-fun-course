fbb79fc39cfd1c028de6926d68b364257f36a546
Tony <tonyyytran@gmail.com>
2018-06-28 09:25:48 -0700

    second activity adding life cycles and instance states

--- unit-1/TwoActivities/app/src/main/java/com/example/tony/twoactivities/SecondActivity.java
+++ unit-1/TwoActivities/app/src/main/java/com/example/tony/twoactivities/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.tony.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "com.example.tony.twoactivities.extra.REPLY";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     private EditText mReply;
 
     @Override
@@ -21,13 +23,53 @@ public class SecondActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
         mReply = findViewById(R.id.enter_message_box_second);
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate2");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart2");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart2");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume2");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause2");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop2");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy2");
     }
 
     public void returnReply(View view) {
+        Log.d(LOG_TAG, "Button clicked2!");
         String reply = mReply.getText().toString();
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
